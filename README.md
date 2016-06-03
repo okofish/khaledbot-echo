15ccbd9432993e35f8718972417f37abe6fb2e9b
Jesse Friedman <jesse@jesse.ws>
2016-06-03 14:42:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# KhaledBot
+KhaledBot is a skill for Amazon Echo that recites one of DJ Khaled's keys to success whenever you feel they don't want you to win.
+
+Inspired by http://khaledbot.com/ by [@IreAderinokun](https://twitter.com/IreAderinokun) and http://www.theydontwantyouto.win/ by [@gk3](https://twitter.com/gk3).  
+Major keys from http://khaledipsum.com/ by [@patrickswong](https://twitter.com/patrickswong).  
+Lion.
---
+++ index.js
@@ -0,0 +1,32 @@
+// KhaledBot for Amazon Echo, inspired by http://khaledbot.com/ and http://www.theydontwantyouto.win/.
+// List of major keys from http://khaledipsum.com/
+
+var alexa = require('alexa-app'),
+  _ = require('underscore'),
+  keys = require('./keys.js');
+
+var app = new alexa.app('KhaledBot');
+
+app.intent('GetKey', {
+  slots: {},
+  utterances: ['{give me |to give me |for |}a {major|} key{ to success|}', '{another |anotha }one']
+}, function(request, response) {
+  var key = _.sample(keys);
+  response.say(key);
+});
+
+app.messages.NO_INTENT_FOUND = 'KhaledBot didn\'t understand that request. Congratulations, you played yourself.'
+
+app.error = function(exception, request, response) {
+    response.say('Sorry, KhaledBot encountered an error. They don\'t want us to win.');
+};
+
+// Checking if we're actually being run as a module (in this case, as a Lambda function.)
+if (module.parent) {
+  exports.handler = app.lambda();
+} else {
+  var fs = require('fs');
+  fs.writeFileSync('schema.json', app.schema());
+  fs.writeFileSync('utterances.txt', app.utterances());
+  console.log('Schema and utterances exported!')
+}
